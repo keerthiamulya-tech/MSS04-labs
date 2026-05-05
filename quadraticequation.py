f53f3dc263b52ac95cff8e43cf23e55e075f3001
Keerthi-Amulya-somaraju <skeerthiamulya@gmail.com>
2026-05-05 12:21:38 +0200

    Update quadraticequation.py

--- quadraticequation.py
+++ quadraticequation.py
@@ -1,1 +1,17 @@
-print('quadratic equation solver')
+import math
+
+def q_solve(a, b, c):
+    if a == 0:
+        return "Not a quadratic equation"
+
+    discriminant = b**2 - 4*a*c
+
+    if discriminant < 0:
+        return "no real solution"
+    elif discriminant == 0:
+        x = -b / (2*a)
+        return (x,)
+    else:
+        x1 = (-b + math.sqrt(discriminant)) / (2*a)
+        x2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return (x1, x2)
